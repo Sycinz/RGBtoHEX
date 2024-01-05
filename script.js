3096a18ea43cd196ea11f56e09bd6f547c2ab729
Sycinz <60658488+Sycinz@users.noreply.github.com>
2024-01-05 17:03:16 +0100

    Create script.js
    Initially committed script

---
+++ script.js
@@ -0,0 +1,13 @@
+const rgbArr = [[243, 12, 85], [231, 43, 59], [92, 56, 37]]
+
+const main = (rgbArr) => {
+    const hexString = '0123456789ABCDEF'
+    var hexResult = '#'
+
+    rgbArr.map(v => { 
+        hexResult += hexString[v !== 0 ? Math.floor(v / 16) : 0] + hexString[v !== 0 ? v % 16 : 0] })
+
+    return hexResult
+}
+
+console.log(main(rgbArr[0]))
